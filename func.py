025372c60fe9b3c170d658b6b630052295580f0f
Diogo Sousa <diogo_k0@hotmail.com>
2020-01-12 23:14:55 -0300

    Primeiro teste de producao para gerar os arquivos

--- func.py
+++ func.py
@@ -12,19 +12,35 @@ conexao = \
 
 def banco_de_dados():
 
+    print('Iniciando processo...')
+
     cnxn = pyodbc.connect(conexao)
+
+    print('Conexão ativa.')
+
     cursor = cnxn.cursor()
     cursor.execute(texto)
+
+    print('Iniciando o carregamento dos arquivos.')
+
     table = cursor.fetchall()
     cnxn.close()
+
+    print('XMLs coletados.')
 
     if os.path.isdir(os.path.expanduser('~') + os.sep + 'Desktop' + os.sep + 'xmls') == True:
         shutil.rmtree(os.path.expanduser('~') + os.sep + 'Desktop' + os.sep + 'xmls')
     os.mkdir(os.path.expanduser('~') + os.sep + 'Desktop' + os.sep + 'xmls')
 
+    print('Escrevendo arquivos.')
+
     for row in table:
 
-        arquivo = open(row.CHAVE + '.txt', 'w')
+        xml = os.path.expanduser('~') + os.sep + 'Desktop' + os.sep + 'xmls' + os.sep + row.CHAVE + '.xml'
+        conteudo = row.XML_Documento
+
+        arquivo = open(xml,'w')
+        arquivo.writelines(conteudo)
         arquivo.close()
 
-    print(len(table))
+    print(str(len(table)) + ' arquivos gerados.')
