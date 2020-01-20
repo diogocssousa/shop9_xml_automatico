e4ed2d66b31c177183b26b827e5e515a08af9fda
Diogo Sousa <diogo_k0@hotmail.com>
2020-01-20 20:52:46 -0300

    Adicinado arquivo conexoes

--- run.py
+++ run.py
@@ -21,46 +21,6 @@ for param in sys.argv :
         continue
 
 
-confs = {
-    'Novo Aviamento':{
-        'habilitado':'yes',
-        'empresa': 'Novo Aviamento',
-        'ip':'186.235.189.90',
-        'porta':'1439',
-        'usuario':'sa',
-        'senha':'Senha123',
-        'titulo':'Novo Aviamento - XML',
-    },
-    'RJ Chaefer': {
-        'habilitado': 'no',
-        'empresa': 'RJ Chaefer',
-        'ip': '45.4.147.69',
-        'porta': '1410',
-        'usuario': 'sa',
-        'senha': 'Senha123',
-        'titulo': 'R&J Chaefer - XML',
-    },
-    'Metropolis Atacado': {
-        'habilitado': 'no',
-        'empresa':'Metropolis Atacado',
-        'ip': '186.235.191.141',
-        'porta': '1434',
-        'usuario': 'sa',
-        'senha': 'Senha123',
-        'titulo': 'Metropolis Moda Urbana - XML',
-    },
-    'Metropolis Varejo': {
-        'habilitado': 'no',
-        'empresa':'Metropolis Varejo',
-        'ip': '186.235.191.141',
-        'porta': '1314',
-        'usuario': 'sa',
-        'senha': 'Senha123',
-        'titulo': 'Metropolis Moda Urbana - XML',
-    },
-}
-
-
 for conf in confs:
 
     if confs[conf]['habilitado'] == 'yes':
